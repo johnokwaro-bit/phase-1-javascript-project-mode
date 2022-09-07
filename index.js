c8f78fd6a1af3233824f57ec1529c8a0ae3d48ba
John Okwaro <john.okwaro@moringaschool.com>
2022-09-07 11:37:27 +0300

    Improved JS

--- index.js
+++ index.js
@@ -3,57 +3,32 @@ function inIt (){
   function fetchFood(dishes){
       const myFetch = fetch("http://localhost:3000/dishes")
       .then(resp => resp.json())
-      .then(dishes => renderDishes(dishes)) 
+      .then(dishes => {
+        // myDishes.append(dishes);
+        renderDishes(dishes)
+      })
 
       return myFetch;    
   }
   
   function renderDishes(dishes){  
     const menuList = document.querySelector('#dish-list')    
-    dishes.forEach(dish =>{
+    dishes.forEach(dish =>{         
       const li = document.createElement("li")      
       li.innerHTML = `<div>
       <img src=${dish.image_url}/>
       <p>${dish.name}</p>
-      <p>${dish.description}      
+      <p>${dish.description}</p>      
       </div>`, 
       menuList.appendChild(li)
-      li.addEventListener("click", function handleEvent(){
-        alert(`Dear customer, you have ordered ${dish.name}. Please indicate your location from search bar for delivery!`)   
-      }) 
-  })
-  }
-
-  function fetchLocation(){
-    const myFetch2 = fetch('http://localhost:3000/locations')
-    .then(response => response.json())
-    .then(data => renderLocation(data))
-
-    return myFetch2
-  }
- 
-  function renderLocation(data){    
-    const userTemplate  = document.querySelector('[user-location-container]')
-    
-    const searchInput = document.querySelector('.input')
-
-    searchInput.addEventListener('input', (e) => {
-      const value = e.target.value
-
+      console.log(menuList)        
+      li.addEventListener("click", (e)=>{
+        alert(`Dear customer, you have ordered ${dish.name}`)
+      })
      
-        
-    })
-    data.forEach(location => {
-      const li = document.createElement('li')
-
-      li.innerHTML = location.name
-      userTemplate.append(li)      
-          
-    })
-    
-  }
-  
-    
+    }
+      )}
+      
   document.getElementById('review-form').addEventListener('submit', event=>{
     event.preventDefault();
     const form = event.target;
